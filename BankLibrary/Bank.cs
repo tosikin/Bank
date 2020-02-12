48a304ffdaff64f07e2131d00e71ad72979c8dd9
Anton Ermalenok <antonermalenok@gmail.com>
2020-02-12 10:23:32 +0200

    metods

--- BankLibrary/Bank.cs
+++ BankLibrary/Bank.cs
@@ -19,9 +19,9 @@ namespace BankLibrary
         {
             this.Name = name;
         }
-            public void Open(AccountType accountType, decimal sum, AccountStateHandler addSumHandler, 
-                 AccountStateHandler withdrawSumHandler, AccountStateHandler calculationHandler,
-                 AccountStateHandler cloceAcoountHandler, AccountStateHandler openStateHandler)
+        public void Open(AccountType accountType, decimal sum, AccountStateHandler addSumHandler, 
+             AccountStateHandler withdrawSumHandler, AccountStateHandler calculationHandler,
+             AccountStateHandler cloceAcoountHandler, AccountStateHandler openStateHandler)
         {
             T newAccount = null;
             switch (accountType)
@@ -38,19 +38,84 @@ namespace BankLibrary
             {
                 throw new Exception("bad account");
             }
-            else
-            {
-                if(accounts == null)
+            
+            if(accounts == null)
                 {
                     accounts = new T[] { newAccount };
                 }
-                else
+            else
                 {
                     T[] tempAccounts = new T[accounts.Length + 1];
-
+                    for (int i = 0; i < accounts.Length; i++)
+                        tempAccounts[i] = accounts[i];
+                    tempAccounts[accounts.Length] = newAccount;
+                    accounts = tempAccounts;
                 }
+                newAccount.Added += addSumHandler;
+                newAccount.Calculated += calculationHandler;
+                newAccount.Closed += cloceAcoountHandler;
+                newAccount.Opened += openStateHandler;
+                newAccount.Withdrawed += withdrawSumHandler;
+                newAccount.Open();
+        }
+
+        public void Put(decimal sum, int id)
+        {
+            T account = FindAccount(id);
+            if (account == null)
+                throw new Exception("Account not found");
+            account.Withdraw(sum);
+        }
+
+        public void Close(int id)
+        {
+            T account = FindAccount(id, out int index);
+            if (account == null)
+                throw new Exception("Account not found");
+            account.Close();
+            if (accounts.Length <= 1)
+                accounts = null;
+            else
+            {
+                T[] tempAccounts = new T[accounts.Length - 1];
+                for(int i = 0, j = 0; i < accounts.Length; i++)
+                {
+                    if (i != index)
+                        tempAccounts[j++] = accounts[i];
+                }
+                accounts = tempAccounts;
             }
         }
+
+
+
+
+        public T FindAccount(int id)
+        {
+            for(int i =0; i < accounts.Length; i++)
+            {
+                if (accounts[i].Id == id)
+                {
+                    return accounts[i];
+                }
+            }
+            return null;
+        }
+
+        public T FindAccount(int id, out int index)
+        {
+            for(int i =0; i < accounts.Length; i++)
+            {
+                if(accounts[i].Id == id)
+                {
+                    index = i;
+                    return accounts[i];
+                }
+            }
+            index = -1;
+            return null;
+        }
+        
 
     }
 }
